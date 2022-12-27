55b09c78f4b35893394b24b5f59105872f0bd578
asazeez <atiyaazeez1@gmail.com>
2022-12-27 01:23:00 -0500

    second commit

--- video_program.py
+++ video_program.py
@@ -1,3 +1,5 @@
+import os
+
 from pytube import YouTube
 import sys
 import video_uploader
@@ -12,13 +14,24 @@ def downloader (url):
         print ('Error')
     print ('successful download')
 
+def video_delete():
+    path = r"C:\Users\atiya\PycharmProjects\video_downloader"
+    for file in os.listdir(path):
+        if "mp4" in file:
+            ans = input("Delete" + file + "? ")
+            if ans == 'yes':
+                try:
+                    os.remove(file)
+                except:
+                    print ('unable to delete file')
 
 if len(sys.argv)>1:
-    print (sys.argv)
-    for x in sys.argv:
-        if "https" in x:
-            downloader(x)
+   print (sys.argv)
+   for x in sys.argv:
+      if "https" in x:
+         downloader(x)
 else:
     sys.exit('enter video url')
 
 video_uploader.google_drive()
+video_delete()
